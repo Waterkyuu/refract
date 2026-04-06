ede583e24e9185abcd3d7a4f8eeeab7ff26ec817
Kerwin Zheng <1747269691@qq.com>
2026-04-07 00:59:46 +0800

    fix(ui): correct footer links and set default button cursor
    fix invalid github urls in home footer social links
    add cursor pointer rule for all button elements
    clean up extra whitespace in global stylesheet

--- components/home/footer.tsx
+++ components/home/footer.tsx
@@ -4,21 +4,21 @@ const Footer = () => {
 	return (
 		<div className="fixed bottom-2 flex w-full items-center justify-between px-4 py-1">
 			<div className="flex items-center gap-2">
-				<a href="https://github/waterkyuu/fire-wave-agent">
+				<a href="https://github.com/waterkyuu/fire-wave-agent">
 					<img
 						src="/images/platform/github.svg"
 						alt="Github logo"
 						className="size-4 md:size-5"
 					/>
 				</a>
-				<a href="https://github/waterkyuu/fire-wave-agent">
+				<a href="https://github.com/waterkyuu/fire-wave-agent">
 					<img
 						src="/images/platform/new-x.svg"
 						alt="X logo"
 						className="size-4 md:size-5"
 					/>
 				</a>
-				<a href="/https://github/waterkyuu/fire-wave-agent">
+				<a href="/https://github.com/waterkyuu/fire-wave-agent">
 					<img
 						src="/images/platform/discord.svg"
 						alt="Discord logo"
